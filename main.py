7d006b3ddcf4884832aeb95f22529b341503ca9e
AironasGit <aironaxas@gmail.com>
2024-09-22 12:54:29 +0300

    Print error if -inputFile flag is not used

--- main.py
+++ main.py
@@ -37,10 +37,14 @@ def main():
     parser.add_argument("-outputPath")
     args = parser.parse_args()
     
+    if args.inputFile == None:
+        print('Error: No file was provided. Please use -inputFile flag')
+        return
+    
     audio_formats = ('mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm')
     text_formats = ('txt')
-    input_file = args.inputFile
-    input_file_name = os.path.basename(args.inputFile).rsplit('.')[0]
+    input_file_path = args.inputFile
+    input_file_name = os.path.basename(input_file_path).rsplit('.')[0]
     output_path = args.outputPath
     
     if output_path == None:
@@ -48,22 +52,21 @@ def main():
     if not os.path.exists(output_path):
         print('Error: Dir does not exist')
         return
-    if not os.path.isfile(input_file):
+    if not os.path.isfile(input_file_path):
         print('Error: Provided file does not exist')
         return
-    if not input_file.endswith(audio_formats) and not input_file.endswith(text_formats):
-        print(f'Error: Wrong file format, please provide one of these: {audio_formats} or {text_formats}')
+    if not input_file_path.endswith(audio_formats) and not input_file_path.endswith(text_formats):
+        print(f'Error: Wrong file format. Please provide one of these: {audio_formats} or {text_formats}')
         return
     
-    
-    if input_file.endswith(audio_formats):
-        text = audio2text(input_file)
+    if input_file_path.endswith(audio_formats):
+        text = audio2text(input_file_path)
         write_result(output_path, f'{input_file_name}_text', text)
         summary = text2summary(text)
         write_result(output_path, f'{input_file_name}_summary', summary)
         print('Done!')
-    elif input_file.endswith(text_formats):
-        text = read_file(input_file)
+    elif input_file_path.endswith(text_formats):
+        text = read_file(input_file_path)
         summary = text2summary(text)
         write_result(output_path, f'{input_file_name}_summary', summary)
         print('Done!')
